f2f71e0f79ba4d86cd0c44b91a1cd2cf25029731
Ladan Salimi <55614277+ladan-salimi@users.noreply.github.com>
2023-01-27 13:36:47 +0100

    Update CQM_classical.py

--- CQM_classical.py
+++ CQM_classical.py
@@ -13,11 +13,6 @@ cqm.add_constraint(2*x+2*y<= 8, "Max perimeter")
 #%%Solver
 sampleset = ExactCQMSolver().sample_cqm(cqm)
 print(sampleset)   
-#SampleSet.filter:Return a new sampleset with rows filtered by the given predicate.
-#the first arument of SampleSet.filter must be a A function that accepts a named tuple as returned by data() and returns a bool.
-#So we use lambda function that is a small anonymous function with syntax of:
-#lambda arguments : expression that The expression is executed and the result is returned:
-#A lambda function can take any number of arguments, but can only have one expression.
 feasible_sampleset = sampleset.filter(lambda row: row.is_feasible)  
 if len(feasible_sampleset): 
 #Sample with the lowest-energy:     
